4de43196e8e8bb2c1354d79b1435bc71f98d00d5
Yaroslav <otebator@gmail.com>
2024-09-20 11:04:14 +1000

    init commit

---
+++ src/main/java/com/example/timetablemicroservice/TimetableMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.timetablemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timetablemicroservice/TimetableMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timetablemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
